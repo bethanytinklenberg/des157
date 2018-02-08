3a96081248edcbd9891cf05211b2ba59a9705049
Bethany <33849458+btinklenberg@users.noreply.github.com>
2018-02-08 04:10:51 -0800

    added some js

--- studios/studio2/scripts.js
+++ studios/studio2/scripts.js
@@ -1,2 +1,29 @@
 //BETHANY TINKLENBERG JS STUDIO 2
 ‘use strict’;
+
+// global variables
+var bottle = document.getElementById('bottle');
+var wallet = document.getElementById('wallet');
+var iphone = document.getElementById('iphone');
+var overlay = document.getElementById('overlay');
+var close = document.getElementById('close');
+
+bottle.addEventListener('ontouchstart',function(){
+  overlay.style.display='block';
+  content.style.display='none';
+});
+
+wallet.addEventListener('ontouchstart',function(){
+  overlay.style.display='block';
+  content.style.display='none';
+});
+
+iphone.addEventListener('ontouchstart',function(){
+  overlay.style.display='block';
+  content.style.display='none';
+});
+
+close.addEventListener('ontouchstart',function(){
+  overlay.style.display='none';
+  content.style.display='block';
+});
