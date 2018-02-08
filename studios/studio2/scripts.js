25ff96d5cbf6a1e8092931036e00d9f9f66c8875
Bethany <33849458+btinklenberg@users.noreply.github.com>
2018-02-08 10:38:42 -0800

    beautify commit

--- studios/studio2/scripts.js
+++ studios/studio2/scripts.js
@@ -8,18 +8,18 @@ var iphone = document.getElementById('iphone');
 var overlay = document.getElementById('overlay');
 var close = document.getElementById('close');
 
-bottle.addEventListener('ontouchstart',function(){
-  bottleoverlay.style.display='block';
+bottle.addEventListener('ontouchstart', function() {
+  bottleoverlay.style.display = 'block';
 });
 
-wallet.addEventListener('ontouchstart',function(){
-  walletoverlay.style.display='block';
+wallet.addEventListener('ontouchstart', function() {
+  walletoverlay.style.display = 'block';
 });
 
-iphone.addEventListener('ontouchstart',function(){
-  iphoneoverlay.style.display='block';
+iphone.addEventListener('ontouchstart', function() {
+  iphoneoverlay.style.display = 'block';
 });
 
-close.addEventListener('ontouchstart',function(){
-  overlay.style.display='none';
+close.addEventListener('ontouchstart', function() {
+  overlay.style.display = 'none';
 });
