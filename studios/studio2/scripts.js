7ccd96d5206f61a13e4e8541fbff99bba438d720
Bethany <33849458+btinklenberg@users.noreply.github.com>
2018-02-08 05:08:41 -0800

    added use strict

--- studios/studio2/scripts.js
+++ studios/studio2/scripts.js
@@ -1,5 +1,5 @@
 //BETHANY TINKLENBERG JS STUDIO 2
-‘use strict’;
+"use strict";
 
 // global variables
 var bottle = document.getElementById('bottle');
@@ -8,23 +8,22 @@ var iphone = document.getElementById('iphone');
 var overlay = document.getElementById('overlay');
 var close = document.getElementById('close');
 
-bottle.addEventListener('click',function(){
-  overlay.style.display='block';
-  bottletext.style.display='block';
-  content.style.display='none';
-});
-
-wallet.addEventListener('click',function(){
+bottle.addEventListener('ontouchstart',function(){
   overlay.style.display='block';
   content.style.display='none';
 });
 
-iphone.addEventListener('click',function(){
+wallet.addEventListener('ontouchstart',function(){
   overlay.style.display='block';
   content.style.display='none';
 });
 
-close.addEventListener('click',function(){
+iphone.addEventListener('ontouchstart',function(){
+  overlay.style.display='block';
+  content.style.display='none';
+});
+
+close.addEventListener('ontouchstart',function(){
   overlay.style.display='none';
   content.style.display='block';
 });
