d311b8aacb1fb5aa5a84afb7d7476474744e766c
Bethany <33849458+btinklenberg@users.noreply.github.com>
2018-02-20 01:06:21 -0800

    mobile working!
    finished studio 2 mobile changes

--- studios/studio2/scripts.js
+++ studios/studio2/scripts.js
@@ -8,18 +8,32 @@ var iphone = document.getElementById('iphone');
 var overlay = document.getElementById('overlay');
 var close = document.getElementById('close');
 
-bottle.addEventListener('ontouchstart', function() {
+
+bottle.addEventListener('click', function() {
   bottleoverlay.style.display = 'block';
+  overlay.style.display = 'block';
+  iphoneoverlay.style.display = 'none';
+  walletoverlay.style.display = 'none';
 });
 
-wallet.addEventListener('ontouchstart', function() {
+wallet.addEventListener('click', function() {
   walletoverlay.style.display = 'block';
+  overlay.style.display = 'block';
+  bottleoverlay.style.display = 'none';
+  iphoneoverlay.style.display = 'none';
+  // close.style.display = 'block';
 });
 
-iphone.addEventListener('ontouchstart', function() {
+iphone.addEventListener('click', function() {
   iphoneoverlay.style.display = 'block';
+  overlay.style.display = 'block';
+  bottleoverlay.style.display = 'none';
+  walletoverlay.style.display = 'none';
 });
 
-close.addEventListener('ontouchstart', function() {
+close.addEventListener('click', function() {
   overlay.style.display = 'none';
+  bottleoverlay.style.display = 'none';
+  iphoneoverlay.style.display = 'none';
+  walletoverlay.style.display = 'none';
 });
