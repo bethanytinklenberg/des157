e82408aa07af444f53ffe6f05f79205b08bd8690
Bethany <33849458+btinklenberg@users.noreply.github.com>
2018-03-11 16:43:55 -0700

    p5.js

--- projects/audience/editorscript.js
+++ projects/audience/editorscript.js
@@ -1,40 +1,32 @@
 //bethany tinklenberg audience test js
 "use strict;"
 
-var slider;
-function setup() {
-  colorMode(HSB);
-  slider = createSlider(0, 360, 60, 40);
-  slider.position(10, 10);
-}
-
-function draw() {
-  var val = slider.value();
-  background(val, 100, 100, 1);
-}
-
-
 // function setup() {
-//   // Create the canvas
-//   createCanvas(720, 400);
-//   background(200);
+//   createCanvas(300,800);
+//   var slider;
+//   colorMode(HSB);
+//   slider = createSlider(0, 360, 60, 40);
+//   slider.position(10, 10);
+// }
 //
-//   // Set colors
-//   fill(204, 101, 192, 127);
-//   stroke(127, 63, 120);
+// function draw() {
+//   var val = slider.value();
+//   background(val, 100, 100, 1);
+// }
+
 //
-//   // A rectangle
-//   rect(40, 120, 120, 40);
-//   // An ellipse
-//   ellipse(240, 240, 80, 80);
-//   // A triangle
-//   triangle(300, 100, 320, 100, 310, 80);
-//
-//   // A design for a simple flower
-//   translate(580, 200);
-//   noStroke();
-//   for (var i = 0; i < 10; i ++) {
-//     ellipse(0, 30, 20, 80);
-//     rotate(PI/5);
-//   }
-// // }
+function setup() {
+  createCanvas(720, 400);
+  background(200);
+
+  // Set colors
+  fill(204, 101, 192, 127);
+  stroke(127, 63, 120);
+
+  translate(580, 200);
+  noStroke();
+  for (var i = 0; i < 10; i ++) {
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+ }
