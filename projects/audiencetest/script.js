17591d95803704f29604c56ab032757853f73934
Bethany <33849458+btinklenberg@users.noreply.github.com>
2018-03-10 23:36:01 -0800

    push to test on iphone

---
+++ projects/audiencetest/script.js
@@ -0,0 +1,6 @@
+//bethany tinklenberg audience test js
+
+addEventListner
+function myFunction() {
+  alert("This webpage is built to be viewed on a mobile phone/tablet. Please open it in the iphone 7 view in the chrome inspector, or on your mobile phone.");
+}
