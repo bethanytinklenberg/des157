3ce8e845354361306037c71a7ca7f48abf9e39eb
Bethany <33849458+btinklenberg@users.noreply.github.com>
2018-03-13 11:20:36 -0700

    remove comps.jpg

--- projects/audiencetest/randomscript.js
+++ projects/audiencetest/randomscript.js
@@ -1,1 +1,10 @@
-//randomscript
+var moveflower = document.getElementById('flower');
+
+
+moveflower.addEventListener('load', function(){
+  for (var i = 0; i < 100; i++) {
+  var r = random(500);
+  translate(r * 5);
+}
+
+})
