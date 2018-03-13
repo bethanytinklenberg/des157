9a00974903bf9743fd7d95c85d5bbd543e08816a
Bethany <33849458+btinklenberg@users.noreply.github.com>
2018-03-13 10:16:50 -0700

    added objectives

--- projects/audiencetest/script.js
+++ projects/audiencetest/script.js
@@ -1,5 +1,5 @@
 //bethany tinklenberg audience test js
 
 function myFunction() {
-  alert("Hello! Thanks for testing my app, I really appreciate it! :) This webpage is built to be viewed on a mobile phone/tablet. Please open it in the iPhone 6/7/8 view in the chrome inspector, or on your mobile phone.");
+  alert("Hello! Thanks for testing my app, I really appreciate it! :) This webpage is built to be viewed on a mobile phone/tablet. Please open it in the iPhone 6/7/8 view in the chrome inspector, or on your mobile phone. Objective: please create a flower, change its color, and submit it.");
 }
