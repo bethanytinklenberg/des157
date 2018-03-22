25ea52ad51fcee9dc081d7f0a5351a2df7fcb500
Bethany <33849458+btinklenberg@users.noreply.github.com>
2018-03-22 05:03:47 -0700

    adding color slider

--- studios/thefinal/indexscript.js
+++ studios/thefinal/indexscript.js
@@ -7,13 +7,14 @@ var petalone = document.getElementById('petal1');
 var petaltwo = document.getElementById('petal2');
 var petalthree = document.getElementById('petal3');
 var showresults = document.getElementById('results');
-var undo = document.getElementById('undo');
 var sizebutton = document.getElementById('sizebutton');
 
 
 petalone.addEventListener('click', function(){
   // showresults.style.display = 'block';
   alert("This is the petal style you have selected");
+  // What I want is for this to display the first flower type. How do I call
+  //that function to be run in the draw loop?
 })
 
 petaltwo.addEventListener('click', function() {
@@ -24,35 +25,72 @@ petalthree.addEventListener('click', function() {
   alert("Awesome, this button works! Unfortunately, its under construction right now.");
 });
 
-undo.addEventListener('click', function() {
-  alert("Awesome, this button works! Unfortunately, its under construction right now.");
-});
-
 sizebutton.addEventListener('click', function() {
   alert("Awesome, this button works! Unfortunately, its under construction right now.");
 });
 
-var slider;
+var rSlider;
+var gSlider;
+var bSlider;
+var mycanvas;
 
 //setting up the canvas
 function setup() {
 
-  var resultCanvas = createCanvas(355, 500);
-  resultCanvas.parent('results');
+  // create canvas
+  mycanvas = createCanvas(355, 500);
+  // mycanvas.position(40,50);
+  mycanvas.parent('results');
+  textSize(10);
   noStroke();
 
-  colorMode(HSB, 350, 100, 85);
-  slider = createSlider(10, 330, 190, 0);
-  slider.parent('colorslider');
-  slider.position(10, 10);
-  slider.style('width', '80px', 'height', '10px',);
-  slider.size(800);
+  // create sliders
+  rSlider = createSlider(0, 255, 100);
+  rSlider.position(25, 420);
+  rSlider.style('background-color','#d31919');
+  rSlider.style('padding', 15 +'px');
+  // rSlider.style('display','none');
+  gSlider = createSlider(0, 255, 0);
+  gSlider.position(25, 450);
+  gSlider.style('background-color','#33a012');
+  gSlider.style('padding', 15 +'px');
+  bSlider = createSlider(0, 255, 255);
+  bSlider.position(25, 480);
+  bSlider.style('background-color','#1458c1');
+  bSlider.style('padding', 15 +'px');
+
 }
 
+// function flowerone(){
+// //this is the color of the flower
+// var val = slider.value();
+// fill(val, 100, 100, 1); // hsba
+// // coloring the flower with the position of the slider
+// //this is the position of the flower in the canvas
+// translate(176, 125);
+// //this is the flower; its an ellipse that is copied and pasted on an axis
+// for (var i = 0; i < 10; i++) {
+//   beginShape();
+//   vertex(0, 0);
+//   vertex(84, 34);
+//   vertex(84, 84);
+//   vertex(34, 84);
+//   endShape(CLOSE);
+//   //this is the axis that it is roated on
+//   rotate(PI / 3);
+//   }
+// }
+
 function draw() {
-  //this is the color of the flower
-  var val = slider.value();
-  fill(val, 100, 100, 1); // hsba
+
+  var r = rSlider.value();
+  var g = gSlider.value();
+  var b = bSlider.value();
+  fill(r, g, b);
+  // text("red", rSlider.x * 2 + rSlider.width, 420);
+  // // console.log('readingjs');
+  // text("green", gSlider.x * 2 + gSlider.width, 460);
+  // text("blue", bSlider.x * 2 + bSlider.width, 480);
   // coloring the flower with the position of the slider
   //this is the position of the flower in the canvas
   translate(176, 125);
@@ -67,7 +105,8 @@ function draw() {
     //this is the axis that it is roated on
     rotate(PI / 3);
     }
-  }
+
+}
 
 
 
