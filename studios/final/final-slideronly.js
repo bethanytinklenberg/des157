16bf10f8583845496b6835c41d9d5699aacf7a10
Bethany <33849458+btinklenberg@users.noreply.github.com>
2018-03-21 20:28:16 -0700

    formatting slider and adjusting sizing for Firefox Browser

--- studios/final/final-slideronly.js
+++ studios/final/final-slideronly.js
@@ -7,7 +7,7 @@ var mycanvas;
 
 function setup() {
   // create canvas
-  mycanvas = createCanvas(900, 800);
+  mycanvas = createCanvas(335, 400);
   // mycanvas.position(40,50);
   mycanvas.parent('coloreditor');
   textSize(50);
@@ -15,13 +15,19 @@ function setup() {
 
   // create sliders
   rSlider = createSlider(0, 255, 100);
-  rSlider.position(200, 820);
-  // rSlider.parent('rslider');
+  rSlider.position(30, 420);
+  rSlider.style('background-color','#d31919');
+  rSlider.style('padding', 15 +'px');
+  // rSlider.parent('colorslider');
   gSlider = createSlider(0, 255, 0);
-  gSlider.position(200, 850);
+  gSlider.position(30, 450);
+  gSlider.style('background-color','#33a012');
+  gSlider.style('padding', 15 +'px');
   // rSlider.parent('gslider');
   bSlider = createSlider(0, 255, 255);
-  bSlider.position(200, 880);
+  bSlider.position(30, 480);
+  bSlider.style('background-color','#1458c1');
+  bSlider.style('padding', 15 +'px');
   // rSlider.parent('bslider');
 }
 
@@ -31,7 +37,8 @@ function draw() {
   var g = gSlider.value();
   var b = bSlider.value();
   background(r, g, b);
-  text("red", rSlider.x * 2 + rSlider.width, 35);
-  text("green", gSlider.x * 2 + gSlider.width, 65);
-  text("blue", bSlider.x * 2 + bSlider.width, 95);
+  text("red", rSlider.x * 2 + rSlider.width, 420);
+  // console.log('readingjs');
+  text("green", gSlider.x * 2 + gSlider.width, 460);
+  text("blue", bSlider.x * 2 + bSlider.width, 480);
 }
